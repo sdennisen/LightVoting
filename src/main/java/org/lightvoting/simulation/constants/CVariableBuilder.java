c2e6204f46d77e1602804510ca5f3268e778a584
Sophie Dennisen <sophie.dennisen@tu-clausthal.de>
2017-02-21 12:32:42 +0100

    add Variable Builder

---
+++ src/main/java/org/lightvoting/simulation/constants/CVariableBuilder.java
@@ -0,0 +1,48 @@
+/**
+ * @cond LICENSE
+ * ######################################################################################
+ * # LGPL License                                                                       #
+ * #                                                                                    #
+ * # This file is part of LightVoting by Sophie Dennisen.                               #
+ * # Copyright (c) 2017, Sophie Dennisen (sophie.dennisen@tu-clausthal.de)              #
+ * # This program is free software: you can redistribute it and/or modify               #
+ * # it under the terms of the GNU Lesser General Public License as                     #
+ * # published by the Free Software Foundation, either version 3 of the                 #
+ * # License, or (at your option) any later version.                                    #
+ * #                                                                                    #
+ * # This program is distributed in the hope that it will be useful,                    #
+ * # but WITHOUT ANY WARRANTY; without even the implied warranty of                     #
+ * # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the                      #
+ * # GNU Lesser General Public License for more details.                                #
+ * #                                                                                    #
+ * # You should have received a copy of the GNU Lesser General Public License           #
+ * # along with this program. If not, see http://www.gnu.org/licenses/                  #
+ * ######################################################################################
+ * @endcond
+ */
+
+package org.lightvoting.simulation.constants;
+
+import org.lightjason.agentspeak.agent.IAgent;
+import org.lightjason.agentspeak.language.execution.IVariableBuilder;
+import org.lightjason.agentspeak.language.instantiable.IInstantiable;
+import org.lightjason.agentspeak.language.variable.CConstant;
+import org.lightjason.agentspeak.language.variable.IVariable;
+import org.lightvoting.simulation.agent.CVotingAgent;
+
+import java.util.stream.Stream;
+
+/**
+ * Created by sophie on 21.02.17.
+ */
+
+final class CVariableBuilder implements IVariableBuilder
+{
+    @Override
+    public final Stream<IVariable<?>> generate( final IAgent<?> p_agent, final IInstantiable p_runningcontext )
+    {
+        return Stream.of(
+            new CConstant<>( "MyName", p_agent.<CVotingAgent>raw().name() )
+        );
+    }
+}
